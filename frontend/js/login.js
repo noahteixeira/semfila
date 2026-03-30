c2396e850773c2cf6eed05f963086f1812f30f55
Lucas Alexandre Schamne <lucasalexandreschamne2022@gmail.com>
2026-03-30 11:59:50 -0300

    v1.0.4

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -3,3 +3,7 @@ var params = new URLSearchParams(window.location.search);
 if (params.get("erro") == "1") {
     document.getElementById("msg-erro").style.display = "block";
 }
+
+if (params.get("desativada") == "1") {
+    document.getElementById("msg-sucesso").style.display = "block";
+}
